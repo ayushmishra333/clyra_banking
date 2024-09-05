8e0f173025628956f9fc0b667f6e8396263b1e43
ayushmishra333 <ayushmishra333@gmail.com>
2024-09-05 23:54:38 +0100

    implement auth, appwrite, and sentry

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link'
 import React from 'react'
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
+
+   console.log(account);
     return (
         <div className='flex flex-col'>
             <Link href="/" className='bank-card'>
